a83e88f152c34b2755793a58077a8762d15bddaa
K. Silva da Visitação <k.silva.da.visitacao@avanade.com>
2019-07-21 21:28:31 -0300

    ajustes seeds

--- seed/index.js
+++ seed/index.js
@@ -5,15 +5,26 @@ const MongoDB = require('./../src/db/strategies/mongodb/mongoDbStrategy')
 const Context = require('./../src/db/strategies/base/contextStrategy')
 const CharacterSchema = require('./../src/db/strategies/mongodb/schemas/CharacterSchema')
 
+const sequentially = (λs) => {
+	return λs.reduce((promise, λ, index) => {
+		return promise.then((items) => {
+			return Promise.resolve(λ(index))
+				.then((item) => [ ...items, item ])
+		});
+	}, Promise.resolve([]));
+}
+
 async function main(){
     const characters = await fsUtil.getFile('db.json')
     const connection = MongoDB.connect()
     await new Context(new MongoDB(connection, CharacterSchema))
-    const savedList = characters.map(async ch =>{
+    const savedList = characters.map((ch) => async () =>{
         let saved = await apiCharacter.create(ch)
         console.log(`Saved: Name: ${saved.name}`)
+        return saved
     })
-    return;
+    await sequentially(savedList)
+    process.exit();
 }
 
 main()
